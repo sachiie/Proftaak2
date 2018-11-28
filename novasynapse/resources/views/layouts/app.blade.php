97b22b8b24a16e5ab9f7e17f1d25206c705856a5
Ridwan <Ridwanslegers4@gmail.com>
2018-11-28 15:08:18 +0100

    jquery link added

--- novasynapse/resources/views/layouts/app.blade.php
+++ novasynapse/resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/hungergames.js') }}" defer></script>
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
     
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
