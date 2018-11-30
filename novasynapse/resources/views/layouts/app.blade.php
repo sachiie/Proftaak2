ce0fc7d187dbe620cd4a3106c421bbdd4a2b76d4
Unknown <naomitjadiwangsa@hotmail.com>
2018-11-30 09:55:24 +0100

    Added default Hunger Games images

--- novasynapse/resources/views/layouts/app.blade.php
+++ novasynapse/resources/views/layouts/app.blade.php
@@ -21,7 +21,6 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    
 </head>
 <body>
     <div id="app">
