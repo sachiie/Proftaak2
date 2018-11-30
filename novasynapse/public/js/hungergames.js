6ae9a4ff7a39956fb249a21fb06c601368b57d77
Ridwan <Ridwanslegers4@gmail.com>
2018-11-30 10:07:39 +0100

    user edits for hunger-games beginning

--- novasynapse/public/js/hungergames.js
+++ novasynapse/public/js/hungergames.js
@@ -1,18 +1,14 @@
 $( document ).ready(function() {
     gameStarted = false;
-    
-
+    var Person = [];
+    var Players = ["Player1", "Player2", "Player3", "Player4", "Player5", "Player6",
+    "Player7", "Player8", "Player9"];
 
 // {name:"", sex:"", image:"", status:"alive"}
 $("#start").click(GenerateRandomUsers);
 
 function GenerateRandomUsers() {
     gameStarted = true;
-    
-    var Players = ["Player1", "Player2", "Player3", "Player4", "Player5", "Player6",
-    "Player7", "Player8", "Player9"];
-
-    var Person = [];
 
     var randomNamesList = ["bob", "marcella", "steve"];
     var randomImageList = ["https://www.ienglishstatus.com/wp-content/uploads/2018/04/Cute-Whatsapp-DP.jpg",
@@ -27,14 +23,21 @@ function GenerateRandomUsers() {
          image = Math.floor(Math.random()*4);
         console.log(name);
         Person[Players[index]] = {
+            "player": index + 1,
             "name" : randomNamesList[name],
             "sex" : randomSex[sex],
             "image" : randomImageList[image],
             "status" : "alive"                  
         };
 
-        $("#player-list").prepend('<img src="' + Person[Players[index]]["image"] + '" alt="..." style="height: 150px;" class="img-thumbnail">');
+        // $("#player-list").prepend('<label for="' + Person[Players[index]]["name"] + '">' + Person[Players[index]]["name"] + '</label>');
+        $("#player-list").append('<img data-toggle="modal" data-target="#myModal" id="' + Person[Players[index]]["player"] + '" name="' + Person[Players[index]]["name"] + '" src="' + Person[Players[index]]["image"] + '" alt="..." style="height: 150px;" class="img-thumbnail player">');
     });
+
+     
+//     $.each(Person, function( index, value ) {
+//        $("#player-list").prepend('<img name="' + Person[Players[index]]["name"] + '" src="' + Person[Players[index]]["image"] + '" alt="..." style="height: 150px;" class="img-thumbnail">');
+//    });
 
     
 if(gameStarted == true){
@@ -44,14 +47,31 @@ console.log(Person);
     
 }
 
+$(document).on('click', ".player", function(){
+    id = $(this).attr("id");
+    $("#player-list").empty();
 
+    $("#submitPlayer").on('click', function(){
+        Person['Player'+id]["name"] =  document.getElementById("name").value;
+        Person['Player'+id]["image"] =  document.getElementById("link").value;
+        Person['Player'+id]["sex"] =  document.getElementById("sex").value;
 
-
-
-
-
-
-
+        $.each(Players, function(index) {
+        
+           Person[Players[index]] = {
+               "player": Person[Players[index]]["Player"],
+               "name" : Person[Players[index]]["name"],
+               "sex" : Person[Players[index]]["sex"],
+               "image" : Person[Players[index]]["image"],
+               "status" : "alive"                  
+           };
+   
+           // $("#player-list").prepend('<label for="' + Person[Players[index]]["name"] + '">' + Person[Players[index]]["name"] + '</label>');
+           $("#player-list").append('<img data-toggle="modal" data-target="#myModal" id="' + Person[Players[index]]["player"] + '" name="' + Person[Players[index]]["name"] + '" src="' + Person[Players[index]]["image"] + '" alt="..." style="height: 150px;" class="img-thumbnail player">');
+       });
+    });
+    console.log(Person);
+});
 
 
 
--- novasynapse/resources/views/hungergameslandingpage.blade.php
+++ novasynapse/resources/views/hungergameslandingpage.blade.php
@@ -10,4 +10,31 @@
     </div>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="myModal" role="dialog">
+<div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+    <div class="modal-header">
+        <h4 class="modal-title">Edit Player</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+    </div>
+    <div class="modal-body">
+        <input type="text" name="" id="name">
+        <input type="text" name="" id="link">
+        <select id="sex">
+                <option value="male">male</option>
+                <option value="female">female</option>
+        </select>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-default" id="submitPlayer" data-dismiss="modal">Submit</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+    </div>
+    </div>
+    
+</div>
+</div>
+
 @endsection
