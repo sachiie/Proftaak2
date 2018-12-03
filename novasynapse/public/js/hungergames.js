b97bc5ff4d6b071093303c3a0cd71aa9d494e865
Ridwan <Ridwanslegers4@gmail.com>
2018-12-03 16:11:16 +0100

    completed hungergames

--- novasynapse/public/js/hungergames.js
+++ novasynapse/public/js/hungergames.js
@@ -1,13 +1,17 @@
 $( document ).ready(function() {
     gameStarted = false;
     $( "#start" ).hide();
+    $( "#next" ).hide();
     var Person = [];
     var Players = ["Player1", "Player2", "Player3", "Player4", "Player5", "Player6",
     "Player7", "Player8", "Player9"];
+    var dayArray = [];
+    var dayCount =1;
 
 // {name:"", sex:"", image:"", status:"alive"}
 $("#generate").click(GenerateRandomUsers);
 $("#start").click(StartGame);
+$("#next").click(Day);
 
 function GenerateRandomUsers() {
     gameStarted = true;
@@ -81,9 +85,175 @@ function StartGame() {
     $("#player-list").empty();
     $( "#start" ).hide()
 
-    var hostileEventsList = ["tripped on" + pronoun + "dick and fell to their death",]
+    // dayCount = 1;
+    // maxDays = 11;
+    // playerCount = 0;
+    // actionCount = 0;
+    // actualDays = 1;
+    // time = 0;
+
+    // // for (dayCount = 1; dayCount < maxDays; dayCount++) {
+    Day();
+    // dayArray.push("its day:" + dayCount++);
+
+    // $.each(Players, function(index) {
+    //     if (Person[Players[index]]["status"] == "alive") {
+    //     eventRoll =  Math.floor(Math.random()*2);
+    //     // console.log(eventRoll);
+    //     // console.log("its day:" + actualDays++);
+    //     switch (eventRoll) {
+    //         case 0:
+    //         hostileEvents(index);
+    //             break;
+
+    //         case 1:
+    //         passiveEvents(index);
+    //             break;
+    //     }
+    // }
+    //     // console.log("the day ends");
+    // });
+    // dayArray.push("the day ends");
+    // }
+
+        // $.each(dayArray, function(index){
+        //     setTimeout(function() {
+
+        //         console.log(dayArray[index]);
+
+        //         if(dayArray.length - 1 === index) {
+        //             console.log('loop ends');
+        //         }
+
+        //     }, time);
+        //     time += 1000;
+        //  });
+        // console.log("starting the next day");
 
 }
+
+function Day(params) {
+    // dayCount = 1;
+    maxDays = 11;
+    playerCount = 0;
+    actionCount = 0;
+    actualDays = 1;
+    time = 0;
+
+    dayArray =[];
+
+    $( "#next" ).hide();
+
+    // for (dayCount = 1; dayCount < maxDays; dayCount++) {
+    
+    dayArray.push("its day:" + dayCount++);
+
+    $.each(Players, function(index) {
+        if (Person[Players[index]]["status"] == "alive") {
+        eventRoll =  Math.floor(Math.random()*2);
+        // console.log(eventRoll);
+        // console.log("its day:" + actualDays++);
+        switch (eventRoll) {
+            case 0:
+            hostileEvents(index);
+                break;
+
+            case 1:
+            passiveEvents(index);
+                break;
+        }
+    }
+        // console.log("the day ends");
+    });
+    dayArray.push("the day ends");
+
+    
+    $.each(dayArray, function(index){
+        setTimeout(function() {
+
+            console.log(dayArray[index]);
+
+            if(dayArray.length - 1 === index) {
+                console.log('loop ends');
+                $( "#next" ).show();
+            }
+
+        }, time);
+        time += 1000;
+     });
+}
+
+function hostileEvents(index){
+        // setTimeout(function() {
+            // dayArray.push("its day:" + actualDays++);
+
+            // $.each(Players, function(index) {
+                    if(Person[Players[index]]["Player"] == "male") {
+
+                        pronoun = "his";
+
+                    }
+                    else {
+
+                        pronoun = "her";
+
+                    }
+
+                    username = Person[Players[index]]["name"];
+
+                    eventRoll =  Math.floor(Math.random()*3);
+
+                    var hostileEventsList = [username + " tripped on " + pronoun + " dick and fell to their death", username + " collided with an asteroid", username + " has encountered aliens and got zapped"];
+
+                    
+                    // dayArray = hostileEventsList[eventRoll];
+                    dayArray.push(hostileEventsList[eventRoll]);
+                    // console.log(dayArray);
+                    Person[Players[index]] = {
+                        "status" : "dead"                  
+                    };
+                
+
+            // });
+
+    };
+
+function passiveEvents(index){
+    // for (dayCount = 1; dayCount < maxDays; dayCount++) {
+
+        // setTimeout(function() {
+            // dayArray.push("its day:" + actualDays++);
+
+            // $.each(Players, function(index) {
+                // if (Person[Players[index]]["status"] == "alive") {
+                    if(Person[Players[index]]["Player"] == "male") {
+
+                        pronoun = "his";
+
+                    }
+                    else {
+
+                        pronoun = "her";
+
+                    }
+
+                    username = Person[Players[index]]["name"];
+
+                    eventRoll =  Math.floor(Math.random()*3);
+
+                    var passiveEventsList = [username + " is taking a nap", username + " doesn't want to get out of bed", username + " is doing a flip", username + " is confused"];
+
+                    
+                    // dayArray = hostileEventsList[eventRoll];
+                    dayArray.push(passiveEventsList[eventRoll]);
+                    // console.log(dayArray);
+                // }
+
+            // });
+
+    };
+
+// }
 
 
 
