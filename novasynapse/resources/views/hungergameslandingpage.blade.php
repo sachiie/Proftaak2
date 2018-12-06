89ceaf847e0f086010b18cfc14dc1defbeff6e74
Unknown <naomitjadiwangsa@hotmail.com>
2018-12-06 10:05:37 +0100

    Hunger games player edit edited

--- novasynapse/resources/views/hungergameslandingpage.blade.php
+++ novasynapse/resources/views/hungergameslandingpage.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid hunger-main">
     <div class="row">
         <div class="col-md-12 hunger-container h-100 flex-column">
-
             {{-- <h1>You</h1>
             <div class="col-6" id="your-list">
                 <img src="css/hunger-images/jon.png">
@@ -15,11 +14,9 @@
             </div> --}}
         </div>
     </div>
-    <div class="row hungry-container mx-auto text">
-            <button id="generate" class="btn bubbly-button">Generate</button>
-            <button id="start" class="btn">Start</button>
-            <button id="next" class="btn">Next day</button>
-            <div class="col-sm your-char h-100">
+    <button id="generate" class="btn bubbly-button w-100">Generate</button>
+    <div class="row hungry-container mx-auto">
+            <div class="col your-char h-100">
                 <div class="col-sm your-id">
                     <div class="container">
                         <div class="row hunger-row">
@@ -41,24 +38,33 @@
                 </div>
             </div>
             <h1></h1>
-            <div class="col-sm">
+            <div class="col">
                 <div class="container flex-row">
                     <div class="row text-center players-row">
-                        <div class="mx-auto" id="player-list">
+                        <div class="col mx-auto h-100" id="player-list">
+                            <div class="col-sm your-players">
                                 <h1 class="">Players</h1>
                                 <div class="player-border">
                                 </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            {{-- <div class="container edit-player">
+                <div class="row">
+                    <img src="css/hunger-images/jon.png">
+                </div>
+            </div> --}}
+
           </div>
+          <button id="next" class="btn mh-100">Next day</button>
+          <button id="start" class="btn w-100 fixed-bottom">Start</button>
 </div>
 
 <!-- Modal -->
 <div class="modal fade" id="myModal" role="dialog">
 <div class="modal-dialog">
-
     <!-- Modal content-->
     <div class="modal-content">
     <div class="modal-header">
@@ -66,19 +72,26 @@
         <button type="button" class="close" data-dismiss="modal">&times;</button>
     </div>
     <div class="modal-body">
-        <input type="text" name="" id="name">
-        <input type="text" name="" id="link">
-        <select id="sex">
-                <option value="male">male</option>
-                <option value="female">female</option>
-        </select>
+        <div class="col-sm text-center" style="max-height: 85px; max-width: 250px;">
+            
+        </div>
+        <div class="container mx-auto">
+            <label>Edit name</label>
+            <input type="text" name="" id="name" class="form-control">
+            <label>Edit image link</label>
+            <input type="text" name="" id="link" class="form-control">
+            <label>Change gender</label>
+            <select id="sex" class="form-control">
+                    <option value="male">male</option>
+                    <option value="female">female</option>
+            </select>
+        </div>
     </div>
     <div class="modal-footer">
         <button type="button" class="btn btn-default" id="submitPlayer" data-dismiss="modal">Submit</button>
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
     </div>
     </div>
-    
 </div>
 </div>
 
